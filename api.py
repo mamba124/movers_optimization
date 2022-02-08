8225e542057a885dfe0ac40d4ca7c4b82dc1ffaf
mamba124 <gipotenuza2017@yandex.ru>
2022-02-09 00:19:38 +0200

    crucial condition

--- api.py
+++ api.py
@@ -32,9 +32,10 @@ if __name__ == '__main__':
                         success = get_opportunity(driver)
                         print(f"Successful? {success}") # TODO when I open a tab I must distinguish tabs and their success
                         logging.info(f"Successful? {success}")
-                        now = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")                        
-                        driver.save_screenshot(f"screens/{now}.png")
-                        driver.close()
+                        if success:
+                            now = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")                      
+                            driver.save_screenshot(f"screens/{now}.png")
+                            driver.close()
                         
             except Exception as e:
                 print(e)
