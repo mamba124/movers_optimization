5efa65280567b8418b1b92b88002415795889c12
mamba124 <gipotenuza2017@yandex.ru>
2022-05-15 17:36:09 +0300

    update constants

--- src/source_work.py
+++ src/source_work.py
@@ -2,17 +2,23 @@ import time
 import json
 from datetime import datetime 
 import logging
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-NEXT_BUTTON = "#modal-portal-container > div:nth-child(2) > div > div > div > div > div.border-color--default__09f24__JbNoB > div > div > div.padding-t4__09f24__Y6aGL.padding-r4__09f24__PQlH_.padding-b4__09f24__q6U6q.padding-l4__09f24__XrHdl.border-color--default__09f24__NPAKY.text-align--left__09f24__ju_Ri > div > div:nth-child(2) > div > button" 
-RADIO_BUTTON = "#how_do_you_want_to_get_more_information--3"
-NAME_SELECTOR = "body > yelp-react-root > div:nth-child(1) > div.messenger-container__09f24__qt8O4 > div > div.messenger_right__09f24__fndbc.border--left__09f24__Lt8WF.border-color--default__09f24__JbNoB > div > div > div.u-flex__09f24__rt07y.u-flex-column__09f24__m6LIn.u-flex-item__09f24__YuSEF.border-color--default__09f24__JbNoB > div.messenger_right_top__09f24__ZxW58.u-padding-t3.u-padding-b3.border--bottom__09f24__Yl28T.border-color--default__09f24__JbNoB > div > div > div > div.arrange-unit__09f24__rqHTg.arrange-unit-fill__09f24__CUubG.border-color--default__09f24__NPAKY > div.user-passport-info.border-color--default__09f24__NPAKY > span > a"
-OPTION_BUTTON = "div.margin-r3__09f24__ppHm0:nth-child(2) > button"
-#SEND_BUTTON = "#modal-portal-container > div:nth-child(2) > div > div > div > div > div.border-color--default__09f24__JbNoB > div > div > div.padding-t4__09f24__Y6aGL.padding-r4__09f24__PQlH_.padding-b4__09f24__q6U6q.padding-l4__09f24__XrHdl.border-color--default__09f24__NPAKY.text-align--left__09f24__ju_Ri > div > div:nth-child(3) > button"
-SEND_BUTTON = "div.margin-r3__09f24__ppHm0:nth-child(2) > button:nth-child(1)"
-ANSWER_BUTTON = ".button--wide__09f24__dKiSe"
+from src.inbox_selectors import NEXT_BUTTON, RADIO_BUTTON, NAME_SELECTOR, \
+                                NEED_MORE_INFO, ANSWER_BUTTON, \
+                                READ_MORE_INBOX, EXPIRED_TIME_QUOTE, \
+                                YELP_WELCOME, LOGO
+
+from src.inbox_selectors import FIRST_EXPIRED, NEXT_ACTIVE #todo add logic that checks if possibility to get fresh quote
+
+
+def check_if_expired_from_page(driver):
+    return "This job has expired" in driver.find_element("css selector", "html").get_attribute("innerHTML")
+
+
 with open("secret_files/user_info.json", "r") as f:
     credentials = json.load(f)
 
@@ -22,8 +28,7 @@ def wait(driver, time, element):
 
 
 def login(driver, link):
-    #WebDriverWait(driver, 6).until(EC.visibility_of_all_elements_located((By.XPATH, "/html/body/yelp-react-root/div/div[2]/div/div[2]/div/div[1]/h1")))
-    wait(driver, 3, ".css-lf8rwb")
+    wait(driver, 3, YELP_WELCOME)
     dog_check(driver)
     email_element = driver.find_elements("name", "email")
     pass_element = driver.find_elements("name", "password")
@@ -32,8 +37,7 @@ def login(driver, link):
         pass_element[0].send_keys(credentials["password"])
         driver.find_elements("tag name", "button")[0].click()
         print(f"Authenticated at time {datetime.now().time()}")
-        element = "#logo > a"
-        wait(driver, 5, element)
+        wait(driver, 5, LOGO)
         time.sleep(2)
         return True
 
@@ -46,12 +50,15 @@ def get_opportunity(driver):
     t2 = None
     print(f"Accessed at {datetime.now().time()}")
     try:
-        driver.find_element("css selector", SEND_BUTTON).click()        
+        driver.find_element("css selector", READ_MORE_INBOX).click()
+        dog_check(driver)
+
+        driver.find_element("css selector", NEED_MORE_INFO).click()
         name = driver.find_elements("css selector", NAME_SELECTOR)
         navigate_through_button_menu(driver)            
-    
+
         message_text = build_message(name)
-        message = driver.find_element("name", "introduce_yourself_send_message")
+        message = driver.find_element("css selector", "#modal-portal-container > div:nth-child(3) > div > div > div > div.border-color--default__09f24__NPAKY > div > div > div.css-40lu3n.padding-t3__09f24__TMrIW.padding-r3__09f24__eaF7p.padding-b4__09f24__q6U6q.padding-l3__09f24__IOjKY.border-color--default__09f24__NPAKY > div.css-fwucvt.margin-b2__09f24__CEMjT.padding-b1__09f24__mrxd5.border-color--default__09f24__NPAKY > div > div.css-iv6qc5.margin-t2__09f24__b0bxj.padding-t2__09f24__Y6duA.border--top__09f24__exYYb.border-color--default__09f24__NPAKY > div.textarea-wrapper__09f24__oXx9N.border-color--default__09f24__NPAKY > textarea")
         message.send_keys(message_text)       
         
         driver.find_element("css selector", ANSWER_BUTTON).click()
@@ -61,10 +68,10 @@ def get_opportunity(driver):
         success = True
     except Exception as ex:
         print(ex)
-        quote_time = driver.find_elements("css selector", "body > yelp-react-root > div > div.messenger-container__09f24__qt8O4 > div > div.messenger_right__09f24__fndbc.border--left__09f24__Lt8WF.border-color--default__09f24__JbNoB > div > div > div.u-flex__09f24__rt07y.u-flex-column__09f24__m6LIn.u-flex-item__09f24__YuSEF.border-color--default__09f24__JbNoB > div.project-description-container__09f24__zySxi.u-flex-item__09f24__YuSEF.messenger-right.border-color--default__09f24__JbNoB > div > div > div.messages-grouped-by-time-view_group_time-sent__09f24__lCCiu.border-color--default__09f24__NPAKY > p")
+        quote_time = driver.find_elements("css selector", EXPIRED_TIME_QUOTE)
         if quote_time:
-            quote_time_string = f"Quote appeared at the time {quote_time[0].text}"
-            t2 = quote_time[0].text
+            t2 = quote_time[0].text.split(": ")[-1]
+            quote_time_string = f"Quote appeared at the time {t2}"
         else:
             quote_time_string = ""
         logging.info(f"Opportunity has expired, no dialogue window found.{quote_time_string}")
@@ -73,14 +80,12 @@ def get_opportunity(driver):
 
 
 def navigate_through_button_menu(driver):
-   # driver.find_element("css selector", OPTION_BUTTON).click()
-    time.sleep(0.2)
     driver.find_element("css selector", RADIO_BUTTON).click()
     time.sleep(0.2)
-   # is_next = driver.find_elements("css selector", NEXT_BUTTON)
-  #  if is_next:
-    #    is_next[0].click()
- #   
+    is_next = driver.find_elements("css selector", NEXT_BUTTON)
+    if is_next:
+        is_next[0].click()
+
 
 def build_message(name):
     if name:
@@ -92,7 +97,7 @@ def build_message(name):
 
 def dog_check(driver):
     try:
-        wait(driver, 8, element="#logo > a")
+        wait(driver, 8, element=LOGO)
     finally:    
         html = driver.find_elements("css selector", "html")[0].get_attribute("outerHTML")
         if "HTTP 504 - GATEWAY TIMEOUT" in html:
